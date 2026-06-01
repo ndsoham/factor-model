729d9c1d6b24b482d3a5494f7073965b2805551a
Soham Nagawanshi <ndsoham@tamu.edu>
2026-06-01 15:48:08 -0500

    Added regression

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from data.fetcher import fetch_factors, fetch_stock_returns
+from model.regression import run_regression
 
-print(fetch_stock_returns("AAPL", "2026-01-01", "2026-03-01"))
-print(fetch_factors("2026-01-01", "2026-03-01").index)
+start_date = "2021-11-09"
+end_date = "2026-06-01"
+ticker = "AAPL"
+
+stock_returns = fetch_stock_returns(ticker, start_date, end_date)
+factors = fetch_factors(start_date, end_date)
+results = run_regression(stock_returns, factors)
+print(results.params)
---
+++ model/regression.py
@@ -0,0 +1,29 @@
+import statsmodels.api as sm
+import pandas as pd
+
+# regression equation
+# excess_return = \alpha + \beta_1 * MktRF + \beta_2 * SMB + \beta_3 * HML + eps
+
+def run_regression(stock_returns: pd.Series, factors: pd.DataFrame) -> any:
+    """
+    Args:
+        stock_returns (pd.Series): pd.Series of daily stock returns named "stock_return"
+        factors (pd.DataFrame): pd.DataFrame with columns Mkt-RF, SMB, HML, RF
+
+    Returns:
+        any: statsmodel RegressionResults object
+    """
+    
+    aligned = pd.concat([stock_returns, factors], axis=1).dropna()
+    aligned["excess"] = aligned["stock_return"] - aligned["RF"]
+    
+    X = aligned.loc[:, ["Mkt-RF", "SMB", "HML"]]
+    X = sm.add_constant(X)
+    
+    y = aligned["excess"]
+    
+    results = sm.OLS(y, X).fit()
+    return results
+    
+    
+    
