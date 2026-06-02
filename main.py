d74b1b0d0d860ff0980da39f5e63fea589a798c1
Soham Nagawanshi <ndsoham@tamu.edu>
2026-06-02 17:11:44 -0500

    Added main

--- main.py
+++ main.py
@@ -2,13 +2,31 @@ from data.fetcher import fetch_factors, fetch_stock_returns
 from model.regression import run_regression
 from metrics.performance import compute_metrics
 from dashboard.app import plot_dashboard
+import argparse
 
-start_date = "2021-11-9"
-end_date = "2026-06-01"
-ticker = "AAPL"
+def parse_args():
+    parser = argparse.ArgumentParser(description="Fama 3 Factor Model")
+    
+    parser.add_argument("--ticker", type=str, default="AAPL")
+    parser.add_argument("--start", type=str, default="2021-11-9")
+    parser.add_argument("--end", type=str, default="2026-05-28")
+    
+    return parser.parse_args()
 
-stock_returns = fetch_stock_returns(ticker, start_date, end_date)
-factors = fetch_factors(start_date, end_date)
-results, aligned = run_regression(stock_returns, factors)
-metrics = compute_metrics(results)
-plot_dashboard(results, metrics, aligned, ticker)
+def main():
+    args = parse_args()
+    
+    stock_returns = fetch_stock_returns(args.ticker, args.start, args.end)
+    factors = fetch_factors(args.start, args.end)
+    results, aligned = run_regression(stock_returns, factors)
+    metrics = compute_metrics(results)
+
+    plot_dashboard(results, metrics, aligned, args.ticker)
+    
+if __name__ == "__main__":
+    main()
+
+
+
+
+
